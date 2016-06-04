6d3d6ef566c3df6e2cbff554074156b80675d61b
Sanjay Kamaruddin <sanjay@sanjaykamaruddin.com>
2016-06-04 00:06:50 -0700

    add client side form validation

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,2 +1,27 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+var validateRegistrationField = function (field) {
+  if (field.checkValidity()) {
+    $(field).removeClass('form-control-danger').addClass('form-control-success').
+      siblings('.text-danger').text('').end().
+      parent('.form-group').addClass('has-success').removeClass('has-danger');
+  } else {
+    $(field).addClass('form-control-danger').removeClass('form-control-success').
+      siblings('.text-danger').text(field.validationMessage).end().
+      parent('.form-group').addClass('has-danger').removeClass('has-success');
+  }
+};
+
+var $registrationFields =  $('#new_user').find('[name^=user]');
+
+$registrationFields.on('change focusout', function (e) {
+  validateRegistrationField(this);
+});
+
+$('#new_user').submit(function (e) {
+  if (this.checkValidity() === false) {
+    $registrationFields.each(function (index, el) {
+      validateRegistrationField(this);
+    });
+    e.preventDefault();
+    return false;
+  }
+});
