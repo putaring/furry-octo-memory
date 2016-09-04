e57d669f295558abf7eb2f8c6bb951cfb9d81e63
Sanjay Kamaruddin <sanjay@sanjaykamaruddin.com>
2016-09-04 15:44:55 -0700

    language

--- app/assets/javascripts/about.js
+++ app/assets/javascripts/about.js
@@ -1,4 +1,4 @@
-document.querySelector('#about-character-count') && 
+document.querySelector('#about-character-count') &&
 (function() {
   var $count      = $('#about-character-count'),
       $aboutInput = $('#profile_about');
