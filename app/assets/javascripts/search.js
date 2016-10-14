609ed0bb757399024240d598379c40054872cbb6
Sanjay Kamaruddin <sanjay@sanjaykamaruddin.com>
2016-10-14 15:59:02 -0700

    search

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -9,3 +9,15 @@ $('[type="checkbox"][name="language"]').change(function (e) {
     $('[type="checkbox"][data-toggle="languages"]').prop('checked', false);
   }
 });
+
+$('[type="checkbox"][data-toggle="countries"]').change(function (e) {
+  if ($(this).is(":checked")) {
+    $('[type="checkbox"][name="country"]').prop('checked', false);
+  }
+});
+
+$('[type="checkbox"][name="country"]').change(function (e) {
+  if ($(this).is(":checked")) {
+    $('[type="checkbox"][data-toggle="countries"]').prop('checked', false);
+  }
+});
