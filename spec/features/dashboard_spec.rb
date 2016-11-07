2ea30717f4a17cdab5a07f52341015971f907c1a
Sanjay Kamaruddin <sanjay@sanjaykamaruddin.com>
2016-11-07 15:42:55 -0800

    fuck the police

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -11,7 +11,7 @@ feature 'Dashboard' do
 
   context "on boarding has not been completed" do
     context "no information has been filled in" do
-      it { should have_content('Get started in 3 easy steps') }
+      it { should have_content("We're so glad to have you on Roozam. Get started in 3 easy steps.") }
       it { should have_content('Add some photos') }
       it { should have_content('Fill out your profile') }
       it { should have_content('Find a match') }
@@ -22,7 +22,7 @@ feature 'Dashboard' do
         user.profile.update_attributes(about: Faker::Lorem.sentence)
         visit me_path
       end
-      it { should have_content('Get started in 3 easy steps') }
+      it { should have_content("We're so glad to have you on Roozam. Get started in 3 easy steps.") }
       it { should have_content('Add some photos') }
       it { should have_content('Fill out your profile') }
       it { should have_content('Find a match') }
@@ -33,7 +33,7 @@ feature 'Dashboard' do
         create(:photo, user: user)
         visit me_path
       end
-      it { should have_content('Get started in 3 easy steps') }
+      it { should have_content("We're so glad to have you on Roozam. Get started in 3 easy steps.") }
       it { should have_content('Add some photos') }
       it { should have_content('Fill out your profile') }
       it { should have_content('Find a match') }
@@ -46,7 +46,7 @@ feature 'Dashboard' do
       user.profile.update_attributes(about: Faker::Lorem.sentence)
       visit me_path
     end
-    it { should have_content('What would you like to do?') }
+    it { should have_content('What would you like to do today?') }
     it { should have_content('Find a match') }
     it { should have_content('View your profile') }
   end
