f3aa38f165128621704c240dc9c090f961915ec2
Sanjay Kamaruddin <sanjay@sanjaykamaruddin.com>
2017-02-15 16:55:15 -0800

    configured application to upload to S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,18 @@
-if Rails.env.test?
+CarrierWave.configure { |config| config.enable_processing = false } if Rails.env.test?
+
+if Rails.env.production?
   CarrierWave.configure do |config|
-    config.enable_processing = false
+    config.storage = :fog
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET'],
+      region: ENV['S3_REGION']
+    }
+    config.fog_directory  = ENV['S3_BUCKET']
+    config.fog_attributes = {'Cache-Control' => 'max-age=315576000, public'}
+    config.cache_dir      = "#{Rails.root}/tmp/uploads"
   end
+else
+  CarrierWave.configure { |config| config.storage = :file }
 end
