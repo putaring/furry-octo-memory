8dacd4f1aa0a236dbba2b9227db787814415fd9b
Sanjay Kamaruddin <sanjay@sanjaykamaruddin.com>
2017-02-23 15:42:57 -0800

    prettify

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,10 +4,10 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.storage = :fog
     config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET'],
-      region: ENV['S3_REGION']
+      provider:               'AWS',
+      aws_access_key_id:      ENV['AWS_ID'],
+      aws_secret_access_key:  ENV['AWS_SECRET'],
+      region:                 ENV['S3_REGION']
     }
     config.fog_directory  = ENV['S3_BUCKET']
     config.fog_attributes = {'Cache-Control' => 'public, max-age=31536000'}
