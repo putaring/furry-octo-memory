53179bc3d9e56d932c70cb85094e3ddeb4385690
Sanjay Kamaruddin <sanjay@sanjaykamaruddin.com>
2017-03-27 17:19:23 -0700

    added bookmark model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170324192535) do
+ActiveRecord::Schema.define(version: 20170328000448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.integer  "bookmarker_id", null: false
+    t.integer  "bookmarked_id", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "bookmarks", ["bookmarked_id"], name: "index_bookmarks_on_bookmarked_id", using: :btree
+  add_index "bookmarks", ["bookmarker_id", "bookmarked_id"], name: "index_bookmarks_on_bookmarker_id_and_bookmarked_id", unique: true, using: :btree
+  add_index "bookmarks", ["bookmarker_id"], name: "index_bookmarks_on_bookmarker_id", using: :btree
 
   create_table "conversations", force: :cascade do |t|
     t.integer  "sender_id",    null: false
