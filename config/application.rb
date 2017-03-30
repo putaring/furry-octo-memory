a9a55eb50ddde84b957551d1b0f40ce91021fb4b
Sanjay Kamaruddin <sanjay@sanjaykamaruddin.com>
2017-03-29 21:23:58 -0700

    setup SES

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,16 @@ module Joonam
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+    
+    config.action_mailer.smtp_settings        = {
+      address:              'email-smtp.us-west-2.amazonaws.com',
+      port:                 587,
+      domain:               'roozam.com',
+      user_name:            ENV['SMTP_USERNAME'],
+      password:             ENV['SMTP_PASSWORD'],
+      authentication:       :login,
+      enable_starttls_auto: true
+    }
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
