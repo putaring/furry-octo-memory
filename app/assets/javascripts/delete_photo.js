0d26ef835b858cb4a95895adb40fd352c8067217
Sanjay Kamaruddin <sanjay@sanjaykamaruddin.com>
2017-04-13 15:49:26 -0700

    likes

--- app/assets/javascripts/delete_photo.js
+++ app/assets/javascripts/delete_photo.js
@@ -2,7 +2,12 @@ $(function() {
   $('body').on('ajax:error', '[data-component="deletePhotoLink"]', function () {
     alert('Oops! something went wrong. Reload the page and try again');
   }).on('ajax:success', '[data-component="deletePhotoLink"]', function () {
-    $(this).parents('.col-md-4').remove();
+    $(this).parents('.col-md-4').slideUp();
+    $.snackbar({
+      content: "Deleted.",
+      style: "snackbar",
+      timeout: 7000
+    });
   }).on('ajax:before', '[data-component="deletePhotoLink"]', function () {
     $(this).addClass('disabled');
   }).on('ajax:complete', '[data-component="deletePhotoLink"]', function () {
