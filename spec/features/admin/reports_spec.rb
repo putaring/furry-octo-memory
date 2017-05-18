0711b66878ec98f0dec4e4f84f06a7075fb156c6
Sanjay Kamaruddin <sanjay@sanjaykamaruddin.com>
2017-05-17 18:45:57 -0700

    la la land

--- spec/features/admin/reports_spec.rb
+++ spec/features/admin/reports_spec.rb
@@ -1,15 +1,17 @@
 require 'rails_helper'
 
-feature "Reports page" do
-  background do
-    create_list(:report, 3)
-    create_list(:resolved_report, 2)
-    visit login_path
-    login(create(:admin_user))
-    visit admin_reports_path
-  end
+feature "Investigate Reports" do
+  context "View Reports" do
+    background do
+      create_list(:report, 3)
+      create_list(:resolved_report, 2)
+      visit login_path
+      login(create(:admin_user))
+      visit admin_reports_path
+    end
 
-  it "should display unresolved reports" do
-    expect(page).to have_selector("tbody tr", count: 3)
+    it "should display unresolved reports" do
+      expect(page).to have_selector("tbody tr", count: 3)
+    end
   end
 end
