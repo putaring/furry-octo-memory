fc4a99ef3e5d20c83f49a56ed92b36173b4735a0
Sanjay Kamaruddin <sanjay@sanjaykamaruddin.com>
2017-06-05 10:17:07 -0700

    don't duplicate that mothaeffin phone number

--- app/models/phone_verification.rb
+++ app/models/phone_verification.rb
@@ -8,6 +8,10 @@ class PhoneVerification < ActiveRecord::Base
 
   validates :code, presence: true, length: { is: 4 }, numericality: { only_integer: true }
 
+  scope :verified, -> { where(verified: true) }
+
+  validate :no_existing_validated_user, on: :create
+
   def send_code
     response = JSON.parse(Net::HTTP.get URI("#{Rails.application.config.otp_api_url}#{phone_number}/#{code}"))
     update_attributes(session_id: response["Details"]) if response["Status"].eql?("Success")
@@ -22,5 +26,9 @@ class PhoneVerification < ActiveRecord::Base
 
   def generate_verification_code
     self.code = Random.rand(1000..9999)
+  end
+
+  def no_existing_validated_user
+    errors.add(:phone_number, "is already linked to a different account") if PhoneVerification.verified.exists?(phone_number: self.phone_number)
   end
 end
