4f1a57d8fc6da161ba15d91d501b4e6becc6c45e
putaring <spouzz.com@gmail.com>
2017-06-24 15:38:36 -0700

    ga. all the way

--- lib/ga.rb
+++ lib/ga.rb
@@ -4,7 +4,6 @@ require 'google/apis/analyticsreporting_v4'
 #OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 Analyticsreporting = Google::Apis::AnalyticsreportingV4
-scopes = [Analyticsreporting::AUTH_ANALYTICS_READONLY]
 service = Analyticsreporting::AnalyticsReportingService.new
-service.authorization = Google::Auth.get_application_default(scopes)
+service.authorization = Google::Auth.get_application_default(Analyticsreporting::AUTH_ANALYTICS_READONLY)
 service.batch_get_reports
