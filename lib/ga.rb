d200bdef6692ce8a83b4b4e3ce42c8ca1a6bc54a
putaring <spouzz.com@gmail.com>
2017-06-27 15:05:53 -0700

    GA shit

--- lib/ga.rb
+++ lib/ga.rb
@@ -1,9 +1,64 @@
+=begin
+r.reports[0].data.rows.first.metrics.first
+=end
+
+
+request = {
+ view_id: "143379915",
+ date_ranges: [
+  {
+   start_date: "2017-06-22",
+   end_date: "2017-06-22"
+  }
+ ],
+ dimensions: [
+  {
+   name: "ga:pagePathLevel2"
+  },
+  {
+   name: "ga:dimension1"
+  }
+ ],
+ metrics: [
+  {
+   expression: "ga:pageViews"
+  }
+ ],
+ dimension_filter_clauses: [
+  {
+   filters: [
+    {
+     dimension_name: "ga:pagePath",
+     expressions: [
+      "^\\/users\\/\\d"
+     ]
+    }
+   ]
+  }
+],
+page_size: 1
+}
+
 require 'googleauth'
 require 'google/apis/analyticsreporting_v4'
-#require 'openssl'
-#OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+require 'openssl'
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 Analyticsreporting = Google::Apis::AnalyticsreportingV4
 service = Analyticsreporting::AnalyticsReportingService.new
 service.authorization = Google::Auth.get_application_default(Analyticsreporting::AUTH_ANALYTICS_READONLY)
-service.batch_get_reports
+
+next_page_token = nil
+begin
+  service.batch_get_reports(Google::Apis::AnalyticsreportingV4::GetReportsRequest.new({report_requests: [request.merge(page_token: next_page_token)]}))
+  report = response.reports.first
+  report.data.rows.each do |row|
+    obj = {
+      profile_id: row.dimensions.first[1..-1],
+      visitor_id: row.dimensions.second,
+      views: row.metrics.first.values.first
+    }
+    puts obj
+  end
+  next_page_token = report.next_page_token
+end while next_page_token
