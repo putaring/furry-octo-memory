e51d09ba35837588d7e39153246892267fb89af2
putaring <spouzz.com@gmail.com>
2017-06-29 12:11:34 -0700

    optimizations, baby

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -10,7 +10,7 @@ class SearchController < ApplicationController
   private
 
   def search
-    @users = User.active.includes(:photos)
+    @users = User.active.includes(:profile_photo)
     filter_gender if params[:gender].present?
     filter_status if params[:status].present?
     filter_age
