b1bc5b392c8f8b126b4ee4262b0a691a7ad29d74
putaring <spouzz.com@gmail.com>
2018-08-31 14:48:01 -0700

    yeah

--- app/jobs/ses/accept_email_job.rb
+++ app/jobs/ses/accept_email_job.rb
@@ -4,15 +4,15 @@ module Ses
     def perform(interest_id)
       interest = Interest.find_by(id: interest_id)
       if interest.present?
-        self.recipient  = interest.liker
-        self.liked      = interest.liked
+        self.recipient  = interest.liked
+        self.liked      = interest.liker
         super()
       end
     end
 
     private
 
-    attr_accessor :liked
+    attr_accessor :liker
 
     def send_email?
       recipient.email_preference.receive_email_notifications?
@@ -24,9 +24,9 @@ module Ses
 
     def template_data
       {
-        liker:              liked.username,
-        liker_avatar_url:   ApplicationController.helpers.avatar_for(liked),
-        liker_url:          user_url(liked)
+        liker:              liker.username,
+        liker_avatar_url:   ApplicationController.helpers.avatar_for(liker),
+        liker_url:          user_url(liker)
       }
     end
   end
--- spec/jobs/ses/accept_email_job_spec.rb
+++ spec/jobs/ses/accept_email_job_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe Ses::AcceptEmailJob, type: :job do
 
   let(:job) { described_class.perform_later(interest_id) }
 
-  context 'when the recipient can receive the email' do
+  context 'when the recipient can receive email notifications' do
     let(:interest_id) { create(:interest, liker: create(:email_preference).user).id }
-    it "send an email to the recipient's inbox" do
+    it "sends the email to the recipient" do
       expect_any_instance_of(Aws::SES::Client).to receive(:send_templated_email)
       perform_enqueued_jobs { job }
     end
@@ -15,7 +15,7 @@ RSpec.describe Ses::AcceptEmailJob, type: :job do
 
   context 'when the recipient cannot receive the email' do
     let(:interest_id) { create(:interest, liker: create(:disabled_notifications_account).user).id }
-    it "doesn't attempt to send the email to the recipient" do
+    it "doesn't send the email to the recipient" do
       expect(Rails.logger).to receive(:warn).with(/^Did not send/)
       perform_enqueued_jobs { job }
     end
